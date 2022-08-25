9d6d49e43e7fcc563ed6fa62bb9f3bec8a034075
duke.liu <401007124@qq.com>
2022-08-25 22:04:33 +0800

    task(master): 接入 SpringBoot

---
+++ mos-web/src/main/java/com/ywj/mos/MOSApplication.java
@@ -0,0 +1,13 @@
+package com.ywj.mos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MOSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MOSApplication.class, args);
+    }
+
+}
